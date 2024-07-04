3119c5dc16068d525e31565052920375bc404e6c
ahttan <94210764+meimei2548@users.noreply.github.com>
2024-07-04 13:03:35 +0700

    tailwind

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ const ShakeGame: FC = () => {
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Shake Game</h1>
+      <h1 className="text-red-800">Shake Game</h1>
       <ShakeComponent/>
       <h3>25 V.4.3</h3>
     </div>
